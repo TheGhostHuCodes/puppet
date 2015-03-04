425a88331e88821760f630c0d113f90c3e42ed32
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-03-04 15:52:49 +0000

    Lint modules.

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,24 +1,24 @@
 class puppet {
-    file { '/usr/local/bin/papply':
-        source => 'puppet:///modules/puppet/papply.sh',
-        mode => '0755',
-    }
+  file { '/usr/local/bin/papply':
+    source => 'puppet:///modules/puppet/papply.sh',
+    mode   => '0755',
+  }
 
-    file { '/usr/local/bin/pull-updates': 
-        source => 'puppet:///modules/puppet/pull-updates.sh',
-        mode => '0755',
-    }
+  file { '/usr/local/bin/pull-updates':
+    source => 'puppet:///modules/puppet/pull-updates.sh',
+    mode   => '0755',
+  }
 
-    cron { 'run-puppet':
-        ensure => 'present',
-        user => 'yhu',
-        command => '/usr/local/bin/pull-updates',
-        minute => '*/15',
-        hour => "*",
-    }
+  cron { 'run-puppet':
+    ensure  => 'present',
+    user    => 'yhu',
+    command => '/usr/local/bin/pull-updates',
+    minute  => '*/15',
+    hour    => '*',
+  }
 
-    package { 'puppet-lint':
-        ensure => '1.1.0',
-        provider => 'gem',
-    }
+  package { 'puppet-lint':
+    ensure   => '1.1.0',
+    provider => 'gem',
+  }
 }
