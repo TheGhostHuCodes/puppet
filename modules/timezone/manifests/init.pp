62c841d0c26ff85ef3a0cfb6620663290e5ff89d
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-03-08 22:59:36 -0700

    Fix timezone module.

--- modules/timezone/manifests/init.pp
+++ modules/timezone/manifests/init.pp
@@ -1,26 +1,32 @@
 class timezone {
-  package { 'tzdata':
-    ensure => installed,
-  }
 }
 
 class timezone::central inherits timezone {
   file { '/etc/localtime':
-    require => Package['tzdata'],
-    source  => 'file:///usr/share/zoneinfo/US/Central',
+    ensure => '/usr/share/zoneinfo/US/Central',
+  }
+
+  file { '/etc/timezone':
+    content => 'America/Chicago',
   }
 }
 
 class timezone::eastern inherits timezone {
   file { '/etc/localtime':
-    require => Package['tzdata'],
-    source  => 'file:///usr/share/zoneinfo/US/Eastern',
+    ensure => '/usr/share/zoneinfo/US/Eastern',
+  }
+
+  file { '/etc/timezone':
+    content => 'America/New_York',
   }
 }
 
 class timezone::pacific inherits timezone {
   file { '/etc/localtime':
-    require => Package['tzdata'],
-    source  => 'file:///usr/share/zoneinfo/US/Pacific',
+    ensure => '/usr/share/zoneinfo/US/Pacific',
+  }
+
+  file { '/etc/timezone':
+    content => 'America/Los_Angeles',
   }
 }
